ece2c0dc86dbfeebba7b6203af4b8a0112eb46cb
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-08-30 19:22:56 +0200

    Fix test

--- ckanext/us_ed_theme/tests/test_helpers.py
+++ ckanext/us_ed_theme/tests/test_helpers.py
@@ -39,25 +39,16 @@ class TestHelpers(HelpersBase, test_helpers.FunctionalTestBase):
         assert result[0]['id'] == dataset['id']
 
     def test_get_groups(self):
+        group1 = factories.Group()
+        result = helpers.get_groups()
+
+        assert len(result) == 1
+
         factories.Group()
-        result = helpers.get_groups()
-
-        assert len(result) == 0
-
-        group = factories.Group()
-        factories.Dataset(groups=[{'id': group['id']}])
-
-        group2 = factories.Group()
-        factories.Dataset(groups=[{'id': group2['id']}])
-        factories.Dataset(groups=[{'id': group2['id']}])
-
-        group3 = factories.Group()
-        factories.Dataset(groups=[{'id': group3['id']}])
-        factories.Dataset(groups=[{'id': group3['id']}])
-        factories.Dataset(groups=[{'id': group3['id']}])
+        factories.Group()
+        factories.Group()
 
         result = helpers.get_groups()
 
-        assert result[0]['id'] == group3['id']
-
-        assert len(result) == 3
+        assert result[0]['id'] == group1['id']
+        assert len(result) == 4
