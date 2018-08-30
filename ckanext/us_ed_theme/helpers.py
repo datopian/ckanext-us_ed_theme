40c62d7bccd64d0d2b6794b255fe78c70230f591
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-08-30 19:22:44 +0200

    Show empty groups as well

--- ckanext/us_ed_theme/helpers.py
+++ ckanext/us_ed_theme/helpers.py
@@ -13,11 +13,9 @@ def get_groups():
     # Helper used on the homepage for showing groups
 
     data_dict = {
-        'sort': 'package_count',
         'all_fields': True
     }
     groups = _get_action('group_list', {}, data_dict)
-    groups = [group for group in groups if group.get('package_count') > 0]
 
     return groups
 
